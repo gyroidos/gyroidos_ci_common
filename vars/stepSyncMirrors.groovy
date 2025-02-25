6d087e3558e0cce9df8c3bff0c1a38a0161d4527
Felix Wruck <felix.wruck@aisec.fraunhofer.de>
2025-02-25 07:28:09 +0100

    vars/stepSyncMirrors: Introduce lock
    This commit introduces a lock for mirror sync operations to avoid
    parallel write operations on the mirror.
    
    Signed-off-by: Felix Wruck <felix.wruck@aisec.fraunhofer.de>

--- vars/stepSyncMirrors.groovy
+++ vars/stepSyncMirrors.groovy
@@ -12,28 +12,30 @@ def call(Map target = [:]) {
 	catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
 		echo "Entering stepSyncMirrors with parameters:\n\tworkspace: ${target.workspace}\n\tssh_cmd: ssh -v\n\tmirror_base_path: ${target.mirror_base_path}\n\tyocto_version: ${target.yocto_version}\n\tgyroid_machine: ${target.gyroid_machine}\n\tbuildtype: ${target.buildtype}"
 
-		sh label: 'Syncing mirrors', script: """
-			cat /home/builder/.ssh/config
+		lock('sync-mirror') {
+			sh label: 'Syncing mirrors', script: """
+				cat /home/builder/.ssh/config
 
-			MIRRORPATH="${target.mirror_base_path}/${target.yocto_version}/${target.gyroid_machine}/"
+				MIRRORPATH="${target.mirror_base_path}/${target.yocto_version}/${target.gyroid_machine}/"
 
-			SSTATE="\$MIRRORPATH/sstate-cache/${target.buildtype}"
-			SOURCES="\$MIRRORPATH/sources/"
-			ATTIC="\$MIRRORPATH/attic_sstate/${target.buildtype}/\$(TZ=UTC date +%Y%m%d_%H%M%S)_${target.buildtype}"
-			TARPATH="\$ATTIC/sstate_${target.buildtype}.tar"
+				SSTATE="\$MIRRORPATH/sstate-cache/${target.buildtype}"
+				SOURCES="\$MIRRORPATH/sources/"
+				ATTIC="\$MIRRORPATH/attic_sstate/${target.buildtype}/\$(TZ=UTC date +%Y%m%d_%H%M%S)_${target.buildtype}"
+				TARPATH="\$ATTIC/sstate_${target.buildtype}.tar"
 
-			ssh -v ${env.MIRRORHOST} "mkdir -p \$SOURCES"
-			ssh -v ${env.MIRRORHOST} "mkdir -p \$ATTIC"
+				ssh -v ${env.MIRRORHOST} "mkdir -p \$SOURCES"
+				ssh -v ${env.MIRRORHOST} "mkdir -p \$ATTIC"
 
-			rsync -v -e "ssh -v" -r --ignore-existing --no-devices --no-specials --no-links "${target.workspace}/out-${target.buildtype}/downloads/" ${env.MIRRORHOST}:"\$SOURCES"
+				rsync -v -e "ssh -v" -r --ignore-existing --no-devices --no-specials --no-links "${target.workspace}/out-${target.buildtype}/downloads/" ${env.MIRRORHOST}:"\$SOURCES"
 
-			ssh -v ${env.MIRRORHOST} "find \$SSTATE -mindepth 1 -maxdepth 1 -exec mv '{}' \$ATTIC \\;"
+				ssh -v ${env.MIRRORHOST} "find \$SSTATE -mindepth 1 -maxdepth 1 -exec mv '{}' \$ATTIC \\;"
 
-			tar -C "${target.workspace}/out-${target.buildtype}/sstate-cache/" -cf "${target.workspace}/sstate_${target.buildtype}.tar" .
+				tar -C "${target.workspace}/out-${target.buildtype}/sstate-cache/" -cf "${target.workspace}/sstate_${target.buildtype}.tar" .
 
-			rsync -v -e "ssh -v" "sstate_${target.buildtype}.tar" ${env.MIRRORHOST}:"\$TARPATH"
+				rsync -v -e "ssh -v" "sstate_${target.buildtype}.tar" ${env.MIRRORHOST}:"\$TARPATH"
 
-			ssh -v ${env.MIRRORHOST} "cd \$SSTATE && tar -xf \$TARPATH"
-		"""
+				ssh -v ${env.MIRRORHOST} "cd \$SSTATE && tar -xf \$TARPATH"
+			"""
+		}
 	}
 }
