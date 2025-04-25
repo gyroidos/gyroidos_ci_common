gyroidos/gyroidos_ci_common

commit 2f0eb4f1fefc2272c24c560cf5b617217a3cb3f4

resources/VM-container-tests: perform extended update test
Added new test case which performs following test to trigger
container reload:

0) signedcontainer1 is stopped.
1) Updated config of "signedcontainer1" with new name
   "signedcontainer1-rename"
   This should directly trigger the reload.

2) Start container by name "signedcontainer1-rename"

3) Updated config of "signedcontainer1-rename" with new name
   "signedcontainer1"
   Since container is running, sync state is set to false but reload
   is not trigged, yet.

4) Stop container by name "signedcontainer1-rename"
   This should trigger the reload.

5) Start container by name "signedcontainer1"
   If reload was sucessfull this should work.

6) Stop container by name "signedcontainer1"

Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

resources/VM-container-commands.sh
@@ -131,6 +131,12 @@ cmd_control_stop() {
 
 }
 
+cmd_control_stop_after_rename() {
+	do_test_cmd_output "/usr/sbin/control stop $1 --key=$3" "CONTAINER_STOP_OK"
+	do_wait_stopped "$2"
+	#sleep 2
+}
+
 cmd_control_stop_error_notrunning() {
 	do_test_cmd_output "/usr/sbin/control stop $1 --key=$2" "CONTAINER_STOP_FAILED_NOT_RUNNING"
 	do_wait_stopped "$1"

resources/VM-container-tests.sh
@@ -101,7 +101,25 @@ do_test_complete() {
 
 	cmd_control_stop_error_notrunning "${CONTAINER}" "$TESTPW"
 
+	# Perform extended update test (triggers reload)
+	if [[ -f "${CONTAINER}_rename.conf" ]]; then
+		# direct reload
+		cmd_control_update_config "${CONTAINER} /tmp/${CONTAINER}_rename.conf /tmp/${CONTAINER}_rename.sig /tmp/${CONTAINER}_rename.cert" "name: \"${CONTAINER}-rename\""
 
+		cmd_control_start "${CONTAINER}-rename" "$TESTPW"
+
+		# indirect reload, trigger internal out-of-sync state
+		cmd_control_update_config "${CONTAINER}-rename /tmp/${CONTAINER}_update.conf /tmp/${CONTAINER}_update.sig /tmp/${CONTAINER}_update.cert" "name: \"${CONTAINER}\""
+
+		# trigger reload
+		cmd_control_stop_after_rename "${CONTAINER}-rename" "${CONTAINER}" "$TESTPW"
+
+		# test if reload worked
+		cmd_control_start "${CONTAINER}" "$TESTPW"
+
+		cmd_control_stop "${CONTAINER}" "$TESTPW"
+	fi
+	#
 	# Perform additional tests in second run
 	if [[ "${SECOND_RUN}" == "y" ]];then
 
@@ -429,6 +447,8 @@ if ! [[ -z "${SCHSM}" ]];then
 	echo_status "VM USB Devices:"
 	ssh ${SSH_OPTS} 'lsusb' 2>&1
 	STAGE="RUN2"
+
+	do_copy_configs
 fi
 
 # List test containers

commit 9c74c0210cb30615bcb6615676257e65736138d8

resources/VM-container-tests: use uuid during config reload test
Instead of using names use the uuid especially to check if the
renamed container is stopped. We switch to cmd_control_stop()
instead of racy cmd_control_stop_after_rename(). Removed command
cmd_control_stop_after_rename() completly since it is not used
anymore.

Fixes: 2f0eb4f1fefc ("resources/VM-container-tests: perform extended update test")
Signed-off-by: Michael Weiß <michael.weiss@aisec.fraunhofer.de>

resources/VM-container-commands.sh
@@ -131,12 +131,6 @@ cmd_control_stop() {
 
 }
 
-cmd_control_stop_after_rename() {
-	do_test_cmd_output "/usr/sbin/control stop $1 --key=$3" "CONTAINER_STOP_OK"
-	do_wait_stopped "$2"
-	#sleep 2
-}
-
 cmd_control_stop_error_notrunning() {
 	do_test_cmd_output "/usr/sbin/control stop $1 --key=$2" "CONTAINER_STOP_FAILED_NOT_RUNNING"
 	do_wait_stopped "$1"

resources/VM-container-tests.sh
@@ -117,18 +117,22 @@ do_test_complete() {
 
 	# Perform extended update test (triggers reload)
 	if [[ -f "${CONTAINER}_rename.conf" ]]; then
-		# direct reload
-		cmd_control_update_config "${CONTAINER} /tmp/${CONTAINER}_rename.conf /tmp/${CONTAINER}_rename.sig /tmp/${CONTAINER}_rename.cert" "name: \"${CONTAINER}-rename\""
+		# get uuid since name is changed during update
+		uuid=$(cmd_control_get_uuid "${CONTAINER}")
 
+		# direct reload
+		cmd_control_update_config "${uuid} /tmp/${CONTAINER}_rename.conf /tmp/${CONTAINER}_rename.sig /tmp/${CONTAINER}_rename.cert" "name: \"${CONTAINER}-rename\""
+
+		# check if reload worked by starting with new name
 		cmd_control_start "${CONTAINER}-rename" "$TESTPW"
 
 		# indirect reload, trigger internal out-of-sync state
-		cmd_control_update_config "${CONTAINER}-rename /tmp/${CONTAINER}_update.conf /tmp/${CONTAINER}_update.sig /tmp/${CONTAINER}_update.cert" "name: \"${CONTAINER}\""
+		cmd_control_update_config "${uuid} /tmp/${CONTAINER}_update.conf /tmp/${CONTAINER}_update.sig /tmp/${CONTAINER}_update.cert" "name: \"${CONTAINER}\""
 
 		# trigger reload
-		cmd_control_stop_after_rename "${CONTAINER}-rename" "${CONTAINER}" "$TESTPW"
+		cmd_control_stop "${uuid}" "$TESTPW"
 
-		# test if reload worked
+		# check if reload worked by starting with old name
 		cmd_control_start "${CONTAINER}" "$TESTPW"
 
 		cmd_control_stop "${CONTAINER}" "$TESTPW"