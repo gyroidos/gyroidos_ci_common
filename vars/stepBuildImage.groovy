gyroidos/gyroidos_ci_common

commit 0bd3f3f68cbd4263c814279dfc35a58bf1f4a6bc

vars/stepBuildImage: Archive bmap + img as tar
Create archives for gyroidosimage.img and gyroidosinstaller.img that
also contain the respective bmap file.

Fixes: b3ad5e6eb73f ("vars/stepBuildImage: add image bmap files to artifacts")
Signed-off-by: Felix Wruck <felix.wruck@aisec.fraunhofer.de>

vars/stepBuildImage.groovy
@@ -111,20 +111,24 @@ def call(Map target) {
 
 	stepStoreRevisions(workspace: target.workspace, buildtype: "${target.buildtype}", manifest_path: target.manifest_path, manifest_name: target.manifest_name, gyroid_machine: target.gyroid_machine)
 
-	sh label: 'Compress gyroidosimage.img', script: "xz -T 0 -f out-${target.buildtype}/tmp/deploy/images/*/gyroidos_image/gyroidosimage.img --keep"
+	sh label: 'Compress gyroidosimage.img', script: """
+		cd ${target.workspace}/out-${target.buildtype}/tmp/deploy/images/*
+		tar -I "xz -T 0" -C gyroidos_image -cf gyroidosimage.tar.xz --dereference gyroidosimage.img gyroidosimage.img.bmap
+	"""
 
 	if (target.containsKey("build_installer") && "y" == target.build_installer) {
-		sh label: 'Compress gyroidosinstaller.img', script: "xz -T 0 -f out-${target.buildtype}/tmp_installer/deploy/images/**/gyroidos_image/gyroidosinstaller.img --keep"
+		sh label: 'Compress gyroidosinstaller.img', script: """
+			cd ${target.workspace}/out-${target.buildtype}/tmp_installer/deploy/images/*
+			tar -I "xz -T 0" -C gyroidos_image -cf gyroidosinstaller.tar.xz --dereference gyroidosinstaller.img gyroidosinstaller.img.bmap
+		"""
 	}
 
 	if (target.containsKey("sync_mirrors") && "y" == target.sync_mirrors) {
 		stepSyncMirrors(workspace: target.workspace, mirror_base_path: target.mirror_base_path, yocto_version: target.yocto_version, gyroid_machine: target.gyroid_machine,  buildtype: target.buildtype, build_number: BUILD_NUMBER)
 	}
 
-	archiveArtifacts artifacts: "out-${target.buildtype}/tmp/deploy/images/**/gyroidos_image/gyroidosimage.img.xz, \
-				       out-${target.buildtype}/tmp/deploy/images/**/gyroidos_image/gyroidosimage.img.bmap, \
-				       out-${target.buildtype}/tmp_installer/deploy/images/**/gyroidos_image/gyroidosinstaller.img.xz, \
-				       out-${target.buildtype}/tmp_installer/deploy/images/**/gyroidos_image/gyroidosinstaller.img.bmap, \
+	archiveArtifacts artifacts: "out-${target.buildtype}/tmp/deploy/images/**/gyroidosimage.tar.xz, \
+				       out-${target.buildtype}/tmp_installer/deploy/images/**/gyroidosinstaller.tar.xz, \
 				       out-${target.buildtype}/test_certificates/**, \
 				       out-${target.buildtype}/tmp/deploy/images/**/ssh-keys/**, \
 				       out-${target.buildtype}/tmp/deploy/images/**/cml_updates/kernel-**.tar, \

vars/stepIntegrationTest.groovy
@@ -13,7 +13,7 @@ def integrationTestX86(Map target = [:]) {
 	step ([$class: 'CopyArtifact',
 		projectName: env.JOB_NAME,
 		selector: target.selector,
-		filter: "out-${target.buildtype}/**/gyroidosimage.img.xz, ${target.source_tarball}",
+		filter: "out-${target.buildtype}/**/gyroidosimage.tar.xz, ${target.source_tarball}",
 		flatten: true]);
 
 
@@ -42,7 +42,7 @@ def integrationTestX86(Map target = [:]) {
 
 	sh "echo \"Unpacking sources\" && tar -C \"${target.workspace}\" -xf ${target.source_tarball}"
 
-	sh label: "Extract image", script: 'unxz -T0 gyroidosimage.img.xz'
+	sh label: "Extract image", script: 'tar -I "xz -T 0" -xf gyroidosimage.tar.xz'
 
 
 	testscript = libraryResource('VM-container-tests.sh')	

commit 3c92cf85ba37af4df7017b619a96c7e573c50009

vars/stepBuildImage: Fix installer compression check
Properly check whether the installer has been built to decide whether it
needs to be compressed for storing it as a build artifact.

Fixes: 0bd3f3f68cbd ("vars/stepBuildImage: Archive bmap + img as tar")
Signed-off-by: Felix Wruck <felix.wruck@aisec.fraunhofer.de>

vars/stepBuildImage.groovy
@@ -116,12 +116,15 @@ def call(Map target) {
 		tar -I "xz -T 0" -C gyroidos_image -cf gyroidosimage.tar.xz --dereference gyroidosimage.img gyroidosimage.img.bmap
 	"""
 
-	if (target.containsKey("build_installer") && "y" == target.build_installer) {
-		sh label: 'Compress gyroidosinstaller.img', script: """
+	sh label: 'Compress gyroidosinstaller.img', script: """
+		if [ -d "${target.workspace}/out-${target.buildtype}/tmp_installer" ];then
+			echo "Compressing installer"
 			cd ${target.workspace}/out-${target.buildtype}/tmp_installer/deploy/images/*
 			tar -I "xz -T 0" -C gyroidos_image -cf gyroidosinstaller.tar.xz --dereference gyroidosinstaller.img gyroidosinstaller.img.bmap
-		"""
-	}
+		else
+			echo "Installer was not built"
+		fi
+	"""
 
 	if (target.containsKey("sync_mirrors") && "y" == target.sync_mirrors) {
 		stepSyncMirrors(workspace: target.workspace, mirror_base_path: target.mirror_base_path, yocto_version: target.yocto_version, gyroid_machine: target.gyroid_machine,  buildtype: target.buildtype, build_number: BUILD_NUMBER)