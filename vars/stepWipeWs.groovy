b7354cc747018e0adff6851f805c26209a0d2c22
Leander Seidlitz <leander.seidlitz@aisec.fraunhofer.de>
2026-04-27 14:11:14 +0200

    vars/stepWipeWs: Improve workspace cleanup
    .manifests* are only ever present on the top level of a workspace.
    We therefore restrict the find, to spare walking the entire directory content.
    Manifests are stashed, the workspace directory cleared and the manifests restored afterwards.
    
    Signed-off-by: Leander Seidlitz <leander.seidlitz@aisec.fraunhofer.de>

--- vars/stepWipeWs.groovy
+++ vars/stepWipeWs.groovy
@@ -1,8 +1,22 @@
 def call(String workspace, String manifest_path) {
-	// params
-	// workspace: Jenkins workspace to wipe
-
-	echo "Entering stepWipeWs with parameter ${workspace}"
-
-	sh "find ${workspace} -mindepth 1 ! -wholename '${manifest_path}*' -print -delete"
+	sh """
+		set -euxo pipefail
+		echo "Clearing '${workspace}' of anything that is not manifests."
+		
+		FILEFILE="\$(mktemp /tmp/gyroid_filelist.XXXXX)"
+		STASHTARBALL="\$(mktemp "\$(dirname "${workspace}")/ws_stash.XXXXXX")"
+		trap 'rm -f "\$FILEFILE" "\$STASHTARBALL"' EXIT
+		
+		# Find all manifest files
+		env -C "${workspace}" find . -mindepth 1 -maxdepth 1 -name '.manifests*' -fprint0 "\$FILEFILE"
+		
+		# Stash them
+		tar --no-dereference -cf "\$STASHTARBALL" -C "${workspace}" --null --files-from="\$FILEFILE"
+		
+		# Wipe workspace contents
+		env -C "${workspace}" find . -mindepth 1 -maxdepth 1 -exec rm -rf {} +
+		
+		# Restore stash
+		tar --no-dereference -xf "\$STASHTARBALL" -C "${workspace}"
+	"""
 }
