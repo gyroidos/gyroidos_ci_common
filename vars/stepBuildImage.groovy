4bfcb14db56169b2c8a3088b85fed1d20faea9cb
Leander Seidlitz <leander.seidlitz@aisec.fraunhofer.de>
2026-05-20 15:14:42 +0200

    vars/stepBuildImage: change datapart size to 12GiB
    Use a more conservative GYROIDOS_DATAPART_EXTRA_SPACE=12GiB for the CI
    pipeline images. Through this change, we drastically lower memory usage
    on the test runners.
    
    Signed-off-by: Leander Seidlitz <leander.seidlitz@aisec.fraunhofer.de>

--- vars/stepBuildImage.groovy
+++ vars/stepBuildImage.groovy
@@ -79,7 +79,7 @@ def call(Map target) {
 
 			MIRRORPATH="${target.mirror_base_path}/${target.yocto_version}/${target.gyroid_machine}/"
 
-			echo 'GYROIDOS_DATAPART_EXTRA_SPACE="20000"' >> conf/local.conf
+			echo 'GYROIDOS_DATAPART_EXTRA_SPACE="12288"' >> conf/local.conf
 
 			echo "INHERIT += \\\"own-mirrors\\\"" >> conf/local.conf
 			echo "SOURCE_MIRROR_URL = \\\"file://${target.mirror_base_path}/sources/\\\"" >> conf/local.conf
